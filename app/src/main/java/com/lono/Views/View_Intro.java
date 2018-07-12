7e57b73f6bc8b1019e71bdd4f3f45a21dcfadb47
felipetorreslinux <felipe.torres.oficial@hotmail.com>
2018-07-11 21:51:07 -0300

    app

--- app/src/main/java/com/lono/Views/View_Intro.java
+++ app/src/main/java/com/lono/Views/View_Intro.java
@@ -2,9 +2,12 @@ package com.lono.Views;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.VideoView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.lono.Firebase.Database;
@@ -14,12 +17,14 @@ public class View_Intro extends Activity implements View.OnClickListener{
 
     Button button_login;
     Button button_register;
+    VideoView videoview_intro;
 
     @Override
     protected void onCreate (Bundle savedInstance){
         super.onCreate(savedInstance);
         setContentView(R.layout.view_intro);
 
+        videoview_intro = (VideoView) findViewById(R.id.videoview_intro);
         button_login = (Button) findViewById(R.id.button_intro_login);
         button_login.setOnClickListener(this);
         button_register = (Button) findViewById(R.id.button_intro_register);
@@ -29,6 +34,16 @@ public class View_Intro extends Activity implements View.OnClickListener{
     @Override
     public void onResume(){
         super.onResume();
+        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.video_intro);
+        videoview_intro.setVideoURI(uri);
+        videoview_intro.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setVolume(0f, 0f);
+                mp.setLooping(true);
+            }
+        });
+        videoview_intro.start();
     }
 
     @Override
