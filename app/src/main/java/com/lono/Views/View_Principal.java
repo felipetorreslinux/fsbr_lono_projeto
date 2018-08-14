b6f6d5bc537f9e70fbe16fa40b92692316521e6f
felipetorreslinux <felipe.torres.oficial@hotmail.com>
2018-08-14 17:31:54 -0300

    app

--- app/src/main/java/com/lono/Views/View_Principal.java
+++ app/src/main/java/com/lono/Views/View_Principal.java
@@ -10,6 +10,8 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -19,7 +21,10 @@ import com.lono.Service.Service_Login;
 
 public class View_Principal extends AppCompatActivity implements View.OnClickListener {
 
+    SharedPreferences.Editor editor;
     Toolbar toolbar;
+    MenuItem exit;
+    MenuItem settings_profile;
 
     LinearLayout item_home;
     LinearLayout item_search;
@@ -32,6 +37,8 @@ public class View_Principal extends AppCompatActivity implements View.OnClickLis
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_principal);
         overridePendingTransition(R.anim.slide_left, R.anim.fade_out);
+
+        editor = getSharedPreferences("profile", MODE_PRIVATE).edit();
 
         infoUserProfile();
 
@@ -74,48 +81,75 @@ public class View_Principal extends AppCompatActivity implements View.OnClickLis
         toolbar.setTitleTextColor(getResources().getColor(R.color.colorWhite));
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_meu_perfil, menu);
+        settings_profile = menu.findItem(R.id.settings_profile);
+        exit = menu.findItem(R.id.exit_app);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.settings_profile:
+                break;
+
+            case R.id.exit_app:
+                editor.putString("token", "");
+                editor.commit();
+                finish();
+                break;
+        }
+        return true;
+    }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.item_home:
                 if(TAB_INDEX != 0){
+                    TAB_INDEX = 0;
                     item_home.setAlpha(1.0f);
                     item_search.setAlpha(0.3f);
                     item_notifi.setAlpha(0.3f);
                     item_person.setAlpha(0.3f);
                     getSupportActionBar().setTitle("Publicações");
-                    TAB_INDEX = 0;
+                    menuPrincipal();
                 }
                 break;
             case R.id.item_search:
                 if(TAB_INDEX != 1){
+                    TAB_INDEX = 1;
                     item_home.setAlpha(0.3f);
                     item_search.setAlpha(1.0f);
                     item_notifi.setAlpha(0.3f);
                     item_person.setAlpha(0.3f);
                     getSupportActionBar().setTitle("Termos e Jornais");
-                    TAB_INDEX = 1;
+                    menuPrincipal();
                 }
                 break;
             case R.id.item_notifi:
                 if(TAB_INDEX != 2){
+                    TAB_INDEX = 2;
                     item_home.setAlpha(0.3f);
                     item_search.setAlpha(0.3f);
                     item_notifi.setAlpha(1.0f);
                     item_person.setAlpha(0.3f);
                     getSupportActionBar().setTitle("Alertas");
-                    TAB_INDEX = 2;
+                    menuPrincipal();
                 }
                 break;
             case R.id.item_person:
                 if(TAB_INDEX != 3){
+                    TAB_INDEX = 3;
                     item_home.setAlpha(0.3f);
                     item_search.setAlpha(0.3f);
                     item_notifi.setAlpha(0.3f);
                     item_person.setAlpha(1.0f);
                     getSupportActionBar().setTitle("Meu Perfil");
-                    TAB_INDEX = 3;
+                    menuPrincipal();
                 }
                 break;
         }
@@ -133,5 +167,27 @@ public class View_Principal extends AppCompatActivity implements View.OnClickLis
         }else{
             finish();
         }
+    }
+
+
+    private void menuPrincipal(){
+         switch (TAB_INDEX){
+             case 0:
+                 settings_profile.setVisible(false);
+                 exit.setVisible(false);
+                 break;
+             case 1:
+                 settings_profile.setVisible(false);
+                 exit.setVisible(false);
+                 break;
+             case 2:
+                 settings_profile.setVisible(false);
+                 exit.setVisible(false);
+                 break;
+             case 3:
+                 settings_profile.setVisible(true);
+                 exit.setVisible(true);
+                 break;
+         }
     }
 }
